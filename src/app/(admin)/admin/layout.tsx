96e24be35e4d0442991368809160284ece45c5e7
imfearnone <121539174+imfearnone@users.noreply.github.com>
2023-11-20 02:19:50 +0800

    2023.11.20.0219

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,3 +1,8 @@
+import Footer from '@/components/footer'
+import Image from "next/image"
+
+
+
 interface IProps {
     children: React.ReactNode
 }
@@ -5,8 +10,16 @@ interface IProps {
 export default function layout({ children }: IProps) {
     return (
         <section>
-            <nav>Hi Im navbar</nav>
+            <nav className="py-10 px-5 md:px-0 flex justify-between items-center container mx-auto">
+                <div>
+                    <Image src={"/vmeme_logo.jpg"} alt="logo" width={150} height={32} />
+                </div>
+                <div className="flex items-center gap-5">
+                    <button>Logout</button>
+                </div>
+            </nav>
             {children}
+            <Footer />
         </section>
     )
 }
