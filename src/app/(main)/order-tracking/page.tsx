1b579d82878e7d1b8daaa855df0d74880f64257f
iamflorenz08 <dejesusflorenz8@gmail.com>
2023-11-29 18:02:17 +0800

    2023.11.29.0602

---
+++ src/app/(main)/order-tracking/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+import { MdDelete } from '@react-icons/all-files/md/MdDelete'
+
+export default function OrderTrackingPage() {
+    return (
+        <main className='container mx-auto xl:px-20 duration-300'>
+
+        </main>
+    );
+}
