59d1a2d678ac2a703e538f1dcd8f7d75b63e0447
iamflorenz08 <dejesusflorenz8@gmail.com>
2023-12-06 21:53:54 +0800

    2023.12.06.0953

--- src/app/(main)/exhibitions/page.tsx
+++ src/app/(main)/exhibitions/page.tsx
@@ -1,38 +1,45 @@
 import { MdNavigateNext } from "@react-icons/all-files/md/MdNavigateNext";
 import { GrFormPrevious } from "@react-icons/all-files/gr/GrFormPrevious";
 import Image from "next/image";
+import Link from "next/link";
 
 interface IExhibits {
     title: string,
     src: string,
     alt: string,
+    href: string
 }
 
 const CurrentExhibits: IExhibits[] = [
     {
         title: 'Bente Singko SenTiMo',
         src: '/bente_singko.webp',
-        alt: 'exhibition'
+        alt: 'exhibition',
+        href: '/benteSingkoSentimo'
     },
     {
         title: 'WOKE UP LIKE THIS',
         src: '/woku_up_like_this.webp',
-        alt: 'exhibition'
+        alt: 'exhibition',
+        href: '/wokeUpLikeThis'
     },
     {
         title: 'ESPIRO',
         src: '/espiro_exh.webp',
-        alt: 'exhibition'
+        alt: 'exhibition',
+        href: '/espiro'
     },
     {
         title: 'The Deliberate Circumstance of Unpredictability',
         src: '/tdcu.webp',
-        alt: 'exhibition'
+        alt: 'exhibition',
+        href: '/'
     },
     {
         title: 'FLOURISH',
         src: '/floral_artist_manila_exh.webp',
-        alt: 'exhibition'
+        alt: 'exhibition',
+        href: '/flourish'
     },
 ]
 
@@ -48,7 +55,11 @@ export default function page() {
                         h-full bg-opacity-50 opacity-0 group-hover:opacity-100 
                         duration-300 flex flex-col gap-5 items-center justify-center text-white">
                             <h1 className="font-black text-xl text-center">{value.title}</h1>
-                            <button className="px-4 py-2 border-2 border-white hover:bg-primary duration-300">View Details</button>
+                            <Link
+                                href={'/exhibitions/' + value.href}
+                                className="px-4 py-2 border-2 border-white hover:bg-primary duration-300">
+                                View Details
+                            </Link>
                         </div>
                     </div>
                 ))}
@@ -56,8 +67,8 @@ export default function page() {
 
             <div className="flex justify-between">
                 <button className='group hover:opacity-80 font-black px-3 py-2 my-14 text-xl xl:text-2xl 2xl:text-3xl text-primary border-4 border-primary tracking-widest flex duration-300'>
-                <span className='opacity-0 group-hover:opacity-100 translate-x-6 group-hover:translate-x-0 duration-300'>
-                    <GrFormPrevious />
+                    <span className='opacity-0 group-hover:opacity-100 translate-x-6 group-hover:translate-x-0 duration-300'>
+                        <GrFormPrevious />
                     </span>
                     <h1>Prev Exhibitions</h1>
                 </button>
@@ -65,7 +76,7 @@ export default function page() {
                 <button className='group hover:opacity-80 font-black px-3 py-2 my-14 text-xl xl:text-2xl 2xl:text-3xl text-primary border-4 border-primary tracking-widest flex duration-300'>
                     <h1>Next Exhibitions</h1>
                     <span className='opacity-0 group-hover:opacity-100 translate-x-6 group-hover:translate-x-0 duration-300'>
-                    <MdNavigateNext />
+                        <MdNavigateNext />
                     </span>
                 </button>
             </div>
